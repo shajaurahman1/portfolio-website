bb54ce3ccaf153625daf196d7df424551f96d454
shajaurahman1 <mohammedshajaurahman@gmail.com>
2025-05-18 21:59:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Welcome To My Portfolio Site!");
+function toggleTheme() {
+  const body = document.body;
+
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+    body.classList.add("light-mode");
+  } else {
+    body.classList.remove("light-mode");
+    body.classList.add("dark-mode");
+  }
+}
+
+window.onload = () => {
+  document.body.classList.add("dark-mode");
+};
